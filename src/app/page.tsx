1370ff8c898908c516386012d892de15cb47a037
TianCimerman <tian.cimerman@gmail.com>
2025-11-27 20:24:07 +0100

    ss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
 // app/page.tsx (This is a Server Component)
 
-// 🚨 Remove 'import dynamic from 'next/dynamic';' and the VantaBackground declaration
-
+import dynamic from 'next/dynamic';
 import ClientVantaWrapper from '../components/ClientVantaWrapper'; // Import the new wrapper
 import Header from '../components/Header.jsx';
-import Introduction from '../components/introduction.jsx';
+
 import Introduction2 from '../components/introduction2.jsx';
 import Projects from '../components/projects.jsx';
 import Contact from '../components/contact.jsx';
+
+
+const Introduction = dynamic(() => import('../components/introduction'), { ssr: false });
 
 export default function HomePage() {
   return (
     // 1. Wrap your entire page content with the Client Wrapper
     <ClientVantaWrapper>
       <Header></Header>
-      <Introduction></Introduction>
+      <Introduction />
 
 
     </ClientVantaWrapper>
