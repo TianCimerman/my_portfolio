db18f533660561beae7176917abdafcf92c801ea
TianCimerman <tian.cimerman@gmail.com>
2025-11-27 19:31:38 +0100

    My portfolio

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  // Step 1 & 2: GitHub Pages export config
+  output: 'export',         // required for static export
+  distDir: 'dist',
+  images:{
+    unoptimized: true,
+  },      // output directory for GitHub Pages
+
+  // Step 3: SVG loader
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
